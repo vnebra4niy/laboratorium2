b714d3f689696eaeb8abf1b1c709c277c756289f
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-03-18 19:23:05 +0100

    Update home.js

--- views/home.js
+++ views/home.js
@@ -1,18 +1,43 @@
 function renderPage() {
     return `
-<html lang=”pl”>
-    <head>
-        <metacharset="UTF-8">
-        <metaname="viewport"content="width=device-width, initial-scale=1.0">
-        <title>Home</title>
+      <html lang="pl">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Formularz</title>
+        </head>
         <body>
-            <div>Home</div>
+          <h2>Prosty formularz</h2>
+          <form action="/submit" method="POST">
+            <label for="name">Imię:</label>
+            <input type="text" id="name" name="name" required><br><br>
+            
+            <label for="lastname">Nazwisko:</label>
+            <input type="text" id="lastname" name="lastname" required><br><br>
+            
+            <label for="age">Wiek:</label>
+            <input type="number" id="age" name="age" required><br><br>
+            
+            <label for="gender">Płeć:</label>
+            <select id="gender" name="gender" required>
+              <option value="male">Mężczyzna</option>
+              <option value="female">Kobieta</option>
+              <option value="other">Inna</option>
+            </select><br><br>
+            
+            <label for="code">Numer albumu:</label>
+            <input type="number" id="code" name="code" required><br><br>
+            
+            <label for="studyField">Kierunek:</label>
+            <input type="text" id="studyField" name="studyField" required><br><br>
+            
+            <button type="submit">Wyślij</button>
+          </form>
         </body>
-    </head>
-</html> 
-`;
-}
-
-module.exports = {
-  renderPage
-};
+      </html>
+    `;
+  }
+  
+  module.exports = {
+    renderPage
+  };
